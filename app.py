aba7fe7c51067bba1f2e1c15ac4e34d920838e78
Kunal6156 <128970613+Kunal6156@users.noreply.github.com>
2025-04-05 17:00:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -947,4 +947,6 @@ def system_status():
     return render_template('status.html', status=status)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
